87efe4799ef11f8bb7072ddcd37257c503f28822
Nate Hughes <nhughes1246@gmail.com>
2018-09-04 14:17:09 -0400

    commit changes to ps_Concatenate-Scripts.ps1

--- ps_Concatenate-Scripts.ps1
+++ ps_Concatenate-Scripts.ps1
@@ -1,5 +1,5 @@
 ﻿#run from cmdline
-#C:\Users\nateh\Projects\powershell-scripts\ps_Concatenate-Scripts.ps1 "D:\SourceSafe\push\DBO\FN" "D:\SourceSafe\push\DBO\2_Combo_FN_VW_SP.sql"
+#D:\GIT\Projects\powershell-scripts\ps_Concatenate-Scripts.ps1 "D:\SourceSafe\push\Prod_Release\DDL" "D:\SourceSafe\push\Prod_Release\1_DDL.sql"
 
 param(
  [string]$inpath,
@@ -11,7 +11,7 @@ $PathArray = @()
 #List file names for all files not containing pattern: "USE "
 Get-ChildItem -path $inpath | ? {$_.PSIsContainer -eq $false} | ? {gc $_.pspath | select-string -pattern "USE "}
 
-#Get-ChildItem -path $inpath -recurse |?{ ! $_.PSIsContainer } |?{($_.name).contains(".sql")} | %{ Out-File -filepath $outpath -inputobject (get-content $_.fullname) -Append}
+#Loop through source directory and append file contents to main script
 Get-ChildItem -path $inpath |?{ ! $_.PSIsContainer } | %{ Out-File -filepath $outpath -inputobject (get-content $_.fullname) -Append}
 
 #Remove ANSI_NULLS and QUOTED_IDENTIFIER overrides so they do not affect other scripts
@@ -19,3 +19,4 @@ Get-ChildItem -path $inpath |?{ ! $_.PSIsContainer } | %{ Out-File -filepath $ou
 (Get-Content $outpath) | Foreach-Object {$_ -replace 'SET ANSI_NULLS OFF',''} | Out-File $outpath
 (Get-Content $outpath) | Foreach-Object {$_ -replace 'SET QUOTED_IDENTIFIER ON',''} | Out-File $outpath
 (Get-Content $outpath) | Foreach-Object {$_ -replace 'SET QUOTED_IDENTIFIER OFF',''} | Out-File $outpath
+(Get-Content $outpath) | Foreach-Object {$_ -replace 'SET XACT_ABORT OFF',''} | Out-File $outpath
