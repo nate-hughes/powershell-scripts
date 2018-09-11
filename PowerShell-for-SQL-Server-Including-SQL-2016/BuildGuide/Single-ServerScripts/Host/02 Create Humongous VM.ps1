8c0c5d4e26d3e738b09fc886b9c05d67314ae63f
Nate Hughes <nhughes1246@gmail.com>
2018-09-11 15:55:24 -0400

    fixing submodule not being tracked

---
+++ PowerShell-for-SQL-Server-Including-SQL-2016/BuildGuide/Single-ServerScripts/Host/02 Create Humongous VM.ps1
@@ -0,0 +1,34 @@
+﻿# *************************************************************************
+#
+# Script Name: Create Humongous VM.ps1
+# PS Version: 5.1
+# Author: Ron Davis
+# 
+# Last Modified: 2/13/2017
+# 
+# Description: This script will create the humongous server virtual machine.
+#              
+#
+# *************************************************************************
+
+#Verify PowerShell is running as Administrator
+$isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+If (!( $isAdmin )) {
+	Write-Host "-- Restarting as Administrator" -ForegroundColor Cyan ; Sleep -Seconds 1
+	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs 
+	exit
+} #END
+
+
+#TASK: CREATE HUMONGOUS VIRTUAL MACHINE
+$SQLISOName = Get-ChildItem -Path 'C:\ISO\SQL\SQL 2016'
+Function Create-Humongous {
+$VMName = "Humongous"
+New-VHD  -Path "C:\RonsNotes\VM_Drives\Humongus\Humongus.vhdx" -SizeBytes 60GB -DynamicMemoryEnabled $false
+New-vm -Name $VMName -MemoryStartupBytes 8GB  -VHDPath "C:\RonsNotes\VM_Drives\Humongus\Humongus.vhdx" -BootDevice IDE -SwitchName "VMExternalNetwork" 
+Set-VMDvdDrive -VMName $VMName -ControllerNumber 1 -ControllerLocation 0 -Path C:\RonsNotes\ISOs\Server2016\14393.0.160715-1616.RS1_RELEASE_SERVER_EVAL_X64FRE_EN-US.ISO
+Set-vm $VMName -ProcessorCount 4 
+Add-VMDvdDrive -VMNAME $VMName -ControllerNumber 1 -ControllerLocation 1 -Path $SQLISOName.Name
+Start-VM  $VMName
+} #END
+Create-Humongous
---
+++ PowerShell-for-SQL-Server-Including-SQL-2016/Lab Files/03 SQL Administration with PowerShell/Logins.ps1
@@ -0,0 +1,99 @@
+﻿#Import the SQL Server module and create the SMO Server object.
+Import-Module SQLPS
+$instanceName = "localhost"
+$server = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Server -ArgumentList $instanceName
+
+<#
+Then we specify what we want returned. We ask for the list of databases 
+and then send them through the pipeline and request the data returned in a list format.
+#>
+
+$server.Logins | Format-List
+
+<#ADDING A NEW LOGIN
+The below lines of code will create a new login for "Mark".
+#>
+$loginName = "Mark"
+$login = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Login -ArgumentList $server, $loginName
+
+<#
+Here we specify the LoginType is SqlLogin and verify PasswordExpirationEnabled is disabled.
+Then we prompt the user for the password, and create the login.
+#>
+$login.LoginType = [Microsoft.SqlServer.Management.Smo.LoginType]::SqlLogin
+$login.PasswordExpirationEnabled = $false
+$pw = Read-Host "Enter preferred password" –AsSecureString
+
+#Next we invoke the Create method of the Login class to create the configured login
+$login.Create($pw)
+
+<#Pause
+Switch to SSMS and refresh Logins folder, then notice 'Mark' is listed.
+#>
+
+<#ADDING A DATABASE USER
+The below lines of code will create a new database user named "Mark" which will map
+to the corresponding server login we created above.
+#>
+$loginName = "Mark"
+
+<#
+This next line of code will retrieve the corresponding server login we would like 
+to map the new database user to.
+#>
+$login = $server.Logins[$loginName]
+
+<#
+Then we configure the database mapping
+#>
+$databasename = "AdventureWorks2014"
+$database = $server.Databases[$databasename]
+
+<#
+Finally, the below code will create the database user.
+#>
+$dbUserName = "Mark"
+$dbuser = New-Object -TypeName Microsoft.SqlServer.Management.Smo.User -ArgumentList $database, $dbUserName
+
+$dbuser.Login = $loginName
+$dbuser.Create()
+
+<#Pause
+Switch to SSMS and refresh Users folder, then notice 'Mark' is listed.
+#>
+
+<#GETTING PERMISSIONS
+The below lines of code will list logins, their assigned roles, database mappings and permissions.
+NOTE: user needs permissions on at least one object w/in DB to show up
+#>
+$databases = @("AdventureWorks2014")
+$results = @()
+
+$databases | ForEach-Object {
+  $database = $server.Databases[$_]
+  $users = $database.Users
+}
+
+#PROPERTIES
+$users |
+  Sort-Object -Property Name |
+  Where-Object IsSystemObject -EQ $false |
+  ForEach-Object {
+    $user  = $_
+
+    #OBJECT PERMISSIONS
+    $database.EnumObjectPermissions($user.Name) | ForEach-Object {
+      $perm = $_
+      $item = [PSCustomObject] @{
+        Login = $user.Login
+        DBUser = $user.Name
+        DBName = $database.Name
+        DBRoles = ($user.EnumRoles())
+        Object = $perm.ObjectName
+        Permission = $perm.PermissionType
+      }
+      $results += $item
+    }
+  }
+
+$results | Format-Table -AutoSize
