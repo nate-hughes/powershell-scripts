40f26c6249cbc56c591ff118fbda306d9e12e58b
Nate Hughes <nate.hughes@bestegg.com>
2021-10-19 13:57:46 -0400

    Latest Mods

---
+++ ps_Concatenate-Date-to-Filename.ps1
@@ -0,0 +1,10 @@
+﻿
+
+$Path = "C:\Temp\"
+"$($Path)Test_$(Get-Date -f yyyyMMdd_HHmmss).txt"
+
+$Path = "C:\Temp"
+[string]::Format(“{0}\{1}_{2}.txt”, $Path, ‘Test’, (Get-Date -f yyyyMMdd_HHmmss))
+
+$Path = "C:\Temp"
+‘{0}\{1}_{2}.txt’ -f $Path, ‘Test’, (Get-Date -f yyyyMMdd_HHmmss)
